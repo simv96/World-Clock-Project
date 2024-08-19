02981ad2300595677050e403e275e9dccee861d9
simv96 <17virdees@gmail.com>
2024-08-19 12:25:31 +0100

    js interval fix

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,36 @@
 function updateTime() {
   let cityOneElement = document.querySelector("#city-one");
-  let cityOneDateElement = cityOneElement.querySelector(".date");
-  let cityOneTimeElement = cityOneElement.querySelector(".time");
-  let cityOneTime = moment().tz("Europe/Rome");
-  cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
-  cityOneTimeElement.innerHTML = cityOneTime.format(
-    "h:mm:ss [<small>] A [</small>]"
-  );
+
+  if (cityOneElement) {
+    let cityOneDateElement = cityOneElement.querySelector(".date");
+    let cityOneTimeElement = cityOneElement.querySelector(".time");
+    let cityOneTime = moment().tz("Europe/Rome");
+    cityOneDateElement.innerHTML = cityOneTime.format("MMMM Do YYYY");
+    cityOneTimeElement.innerHTML = cityOneTime.format(
+      "h:mm:ss [<small>] A [</small>]"
+    );
+  }
   let cityTwoElement = document.querySelector("#city-two");
-  let cityTwoDateElement = cityTwoElement.querySelector(".date");
-  let cityTwoTimeElement = cityTwoElement.querySelector(".time");
-  let cityTwoTime = moment().tz("Europe/Dublin");
-  cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
-  cityTwoTimeElement.innerHTML = cityTwoTime.format(
-    "h:mm:ss [<small>] A [</small>]"
-  );
+
+  if (cityTwoElement) {
+    let cityTwoDateElement = cityTwoElement.querySelector(".date");
+    let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+    let cityTwoTime = moment().tz("Europe/Dublin");
+    cityTwoDateElement.innerHTML = cityTwoTime.format("MMMM Do YYYY");
+    cityTwoTimeElement.innerHTML = cityTwoTime.format(
+      "h:mm:ss [<small>] A [</small>]"
+    );
+  }
   let cityThreeElement = document.querySelector("#city-three");
-  let cityThreeDateElement = cityThreeElement.querySelector(".date");
-  let cityThreeTimeElement = cityThreeElement.querySelector(".time");
-  let cityThreeTime = moment().tz("America/Toronto");
-  cityThreeDateElement.innerHTML = cityThreeTime.format("MMMM Do YYYY");
-  cityThreeTimeElement.innerHTML = cityThreeTime.format(
-    "h:mm:ss [<small>] A [</small>]"
-  );
+  if (cityThreeElement) {
+    let cityThreeDateElement = cityThreeElement.querySelector(".date");
+    let cityThreeTimeElement = cityThreeElement.querySelector(".time");
+    let cityThreeTime = moment().tz("America/Toronto");
+    cityThreeDateElement.innerHTML = cityThreeTime.format("MMMM Do YYYY");
+    cityThreeTimeElement.innerHTML = cityThreeTime.format(
+      "h:mm:ss [<small>] A [</small>]"
+    );
+  }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
@@ -39,9 +47,7 @@ function updateCity(event) {
           )} <small>${citySelectTime.format("A")}</small></div>
         </div>`;
 }
-
-updateTime();
-//setInterval(updateTime, 1000);
-
 let citiesSelectElement = document.querySelector(".list");
 citiesSelectElement.addEventListener("change", updateCity);
+updateTime();
+//setInterval(updateTime, 1000);
